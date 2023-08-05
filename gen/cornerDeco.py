6cf0ce9e07fd0b3340b063b5e162b78e8f10f0bc
levtus <61996958+alexandsr@users.noreply.github.com>
2023-08-05 10:18:47 +0200

    use loops to remove duplicate code.

--- gen/cornerDeco.py
+++ gen/cornerDeco.py
@@ -4,17 +4,29 @@ image = PIL.Image.open("gen/input/topleft_fixed_resolution.png")
 pixels = image.load()
 width, height = image.size  
 
-with open("gen/output/deco_fix.lua", "w") as f:    
+with open("gen/output/deco_fix.lua", "w") as f:
+    yValuesOrange = []
+    xValuesOrange = []
+    yValuesYellow = []
+    xValuesYellow = []
     for y in range(height):         
         for x in range(width):
             print(f"({x}, {y}) = {pixels[x, y]}")
-            if pixels[x, y] == 2:                
-                f.write(f"paintutils.drawPixel({x + 2}, {y + 2}, colors.orange)\n")
-                f.write(f"paintutils.drawPixel(width - {(x + 1)}, {y + 2}, colors.orange)\n")
-                f.write(f"paintutils.drawPixel({(x + 2)}, height - {y + 1}, colors.orange)\n")
-                f.write(f"paintutils.drawPixel(width - {(x + 1)}, height - {y + 1}, colors.orange)\n")
+            if pixels[x, y] == 2:           
+                xValuesOrange.append(x)
+                yValuesOrange.append(y)
+                # f.write(f"paintutils.drawPixel({x + 2}, {y + 2}, colors.orange)\n")
+                # f.write(f"paintutils.drawPixel(width - {(x + 1)}, {y + 2}, colors.orange)\n")
+                # f.write(f"paintutils.drawPixel({(x + 2)}, height - {y + 1}, colors.orange)\n")
+                # f.write(f"paintutils.drawPixel(width - {(x + 1)}, height - {y + 1}, colors.orange)\n")
             elif pixels[x, y] == 1:
-                f.write(f"paintutils.drawPixel({x + 2}, {y + 2}, colors.yellow)\n")
-                f.write(f"paintutils.drawPixel(width - {(x + 1)}, {y + 2}, colors.yellow)\n")
-                f.write(f"paintutils.drawPixel({(x + 2)}, height - {y + 1}, colors.yellow)\n")
-                f.write(f"paintutils.drawPixel(width - {(x + 1)}, height - {y + 1}, colors.yellow)\n")
+                xValuesYellow.append(x)
+                yValuesYellow.append(y)
+                # f.write(f"paintutils.drawPixel({x + 2}, {y + 2}, colors.yellow)\n")
+                # f.write(f"paintutils.drawPixel(width - {(x + 1)}, {y + 2}, colors.yellow)\n")
+                # f.write(f"paintutils.drawPixel({(x + 2)}, height - {y + 1}, colors.yellow)\n")
+                # f.write(f"paintutils.drawPixel(width - {(x + 1)}, height - {y + 1}, colors.yellow)\n")
+    f.write(f"xValuesDecoOrange = {{ {', '.join(map(str, xValuesOrange))[1:-1]} }}\n")
+    f.write(f"yValuesDecoOrange = {{ {', '.join(map(str, yValuesOrange))[1:-1]} }}\n")
+    f.write(f"xValuesDecoYellow = {{ {', '.join(map(str, xValuesYellow))[1:-1]} }}\n")
+    f.write(f"yValuesDecoYellow = {{ {', '.join(map(str, yValuesYellow))[1:-1]} }}\n")
--- gen/stress.py
+++ gen/stress.py
@@ -4,10 +4,22 @@ pixels = image.load()
 width, height = image.size
 
 with open(f"gen/output/stress.lua", "w") as f:    
+    yValuesGray = []
+    xValuesGray = []
+    yValuesLightGray = []
+    xValuesLightGray = []
     for y in range(height):         
         for x in range(width):
             print(f"({x}, {y}) = {pixels[x, y]}")
-            if pixels[x, y] == 0 or pixels[x, y] == (0, 0, 0, 255):                
-                f.write(f"paintutils.drawPixel({x-(0.5*width)} + (0.5*width) + 2, {y + 28}, colors.gray)\n")
+            if pixels[x, y] == 0 or pixels[x, y] == (0, 0, 0, 255):
+                xValuesGray.append(x)
+                yValuesGray.append(y)
+                # f.write(f"paintutils.drawPixel({x-(0.5*width)} + (0.5*width) + 2, {y + 28}, colors.gray)\n")
             elif pixels[x, y] == 1 or pixels[x, y] == (128, 128, 128, 255):
-                f.write(f"paintutils.drawPixel({x-(0.5*width)} + (0.5*width) + 2, {y + 28}, colors.lightGray)\n")
+                xValuesLightGray.append(x)
+                yValuesLightGray.append(y)
+                # f.write(f"paintutils.drawPixel({x-(0.5*width)} + (0.5*width) + 2, {y + 28}, colors.lightGray)\n")
+    f.write(f"xValuesStressGray = {{ {', '.join(map(str, xValuesGray))[1:-1]} }}\n")
+    f.write(f"yValuesStressGray = {{ {', '.join(map(str, yValuesGray))[1:-1]} }}\n")
+    f.write(f"xValuesStressLightGray = {{ {', '.join(map(str, xValuesLightGray))[1:-1]} }}\n")
+    f.write(f"yValuesStressLightGray = {{ {', '.join(map(str, yValuesLightGray))[1:-1]} }}\n")
